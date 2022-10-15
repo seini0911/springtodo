96768516dd3be48c480c759bc95155508454bdf0
unknown <seiniabaya@gmail.com>
2022-10-15 23:15:35 +0100

    first commit of the project

---
+++ src/main/java/com/moobifundev/springboottodo/SpringbootToDoApplication.java
@@ -0,0 +1,13 @@
+package com.moobifundev.springboottodo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootToDoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootToDoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/moobifundev/springboottodo/SpringbootToDoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.moobifundev.springboottodo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootToDoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
